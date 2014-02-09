0031c59c593b5742993c3e10d6fbb043a84f3289
Chris Plock <plockc@gmail.com>
2014-02-09 13:22:51 -0800

    nicer api

--- plock/util/CompileSourceInMemory.java
+++ plock/util/CompileSourceInMemory.java
@@ -10,36 +10,80 @@ import javax.tools.JavaCompiler.CompilationTask;
 import javax.tools.JavaFileObject.Kind;
 
 // TODO: want hook for getting back classname/class bytes/method or something so can cache
+// TODO: create a Class concreteImplementation(Class c) where c is an interface with a single method
+// TODO: transform run to use the above and require a no-arg method
+// TODO: Builder pattern of TemplateInstances with defaults for stuff like imports
 public class CompileSourceInMemory {
   static final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 
   public static void main(String args[]) throws Exception {
+      System.out.println(createSimpleImplCode(Runnable.class, "System.out.println(\"slow\");\nSystem.out.println(\"super\");"));
     runJavaFragment("System.out.println(\"slow\");\nSystem.out.println(\"super\");");
     runJavaFragment("System.out.println(\"fast\");");
   }
-  // TODO: this seems a little awkward, consider future of supporting methods, instance vars, etc.
-  public static Object runJavaFragment(final String code) throws Exception {
-      runJavaMethod("execute", "public static void execute() {\n"+code+"\n}");
-      return null;
+  public static void runJavaFragment(final String code) throws Exception {
+        Runnable runnable = createSimpleInstance(Runnable.class, code);
+        runnable.run();
   }
-  public static Object runJavaMethod(final String methodName, final String code) throws Exception {
-    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-    final String name = "TransientClassCompiledInMemory";
-    //final String name = "HelloWorld"+Math.abs(new java.util.Random().nextInt());
-    URI uri = URI.create("string:///" + name.replace('.','/') + Kind.SOURCE.extension);
-    SimpleJavaFileObject source = new SimpleJavaFileObject(uri, Kind.SOURCE) {
-      public CharSequence getCharContent(boolean ignoreEncodingErrors) { 
-        return "public class "+name+" {\n"+code+"\n}";
+  public static <T> T createSimpleInstance(Class<T> interfaceClass, String code) throws Exception {
+        Class<T> clazz = createSimpleImpl(interfaceClass, code);
+        Constructor<?> constructor = clazz.getConstructor(new Class[] {});
+        return (T)constructor.newInstance(new Object[] {});
+  }
+  public static <T> Class<T> createSimpleImpl(Class<T> interfaceClass, String codeFragment) throws Exception {
+      StringBuilder className = new StringBuilder();
+      String packageName = interfaceClass.getPackage().getName();
+      if (packageName.equals("java.lang")) {
+          packageName="";
       }
+      if (packageName.length() > 0) {
+          className.append(interfaceClass.getPackage().getName()).append('.');
+      }
+      className.append("Dynamic"+interfaceClass.getSimpleName()+"Impl");
+      return createClass(className.toString(), createSimpleImplCode(interfaceClass, codeFragment));
+  }
+  public static <T> String createSimpleImplCode(Class<T> interfaceClass, String codeFragment) {
+      String interfaceName = interfaceClass.getSimpleName();
+      Method[] methods = interfaceClass.getMethods();
+      if (!interfaceClass.isInterface() || methods.length != 1) {
+          throw new IllegalArgumentException("must use an interface with only a single method");
+      }
+      Method method = methods[0];
+      String name = method.getName();
+      StringBuilder classCode = new StringBuilder();
+      String packageName = interfaceClass.getPackage().getName();
+      if (packageName.equals("java.lang")) {
+          packageName="";
+      }
+      if (packageName.length() > 0) {
+          classCode.append("package "+interfaceClass.getPackage().getName()+";\n");
+      }
+      classCode.append("public class Dynamic"+interfaceName+"Impl implements ");
+      classCode.append(interfaceName+" {\n");
+      classCode.append("public "+method.getReturnType().getName()+" "+method.getName()+"(");
+      int argNum=0;
+      for (Class argType : method.getParameterTypes()) {
+          classCode.append(argType.getName()).append(" ").append("arg"+argNum++);
+      }
+      classCode.append(") {\n");
+      classCode.append(codeFragment);
+      classCode.append("}\n}");
+      return classCode.toString();
+  }
+  public static <T> Class<T> createClass(String fullClassName, final String classCode) throws Exception {
+    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    URI uri = URI.create("string:///" + fullClassName.replace('.','/') + Kind.SOURCE.extension);
+    SimpleJavaFileObject source = new SimpleJavaFileObject(uri, Kind.SOURCE) {
+      public CharSequence getCharContent(boolean ignoreEncodingErrors) { return classCode; } 
     };
     Iterable<? extends JavaFileObject> sources = Arrays.asList(source);
     DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
     JavaFileManager standardFileManager = compiler.getStandardFileManager(null, null, null); 
     final ByteArrayOutputStream classBytesStream = new ByteArrayOutputStream();
     JavaFileManager fileManager = new ForwardingJavaFileManager<JavaFileManager>(standardFileManager) {
-      public JavaFileObject getJavaFileForOutput(Location location, String className, Kind kind,
+      public JavaFileObject getJavaFileForOutput(Location location, String fullClassName, Kind kind,
               FileObject sibling) throws IOException {
-            URI classUri = URI.create("string:///" + name.replace('.', '/') + kind.extension);
+            URI classUri = URI.create("string:///" + fullClassName.replace('.', '/') + kind.extension);
             return new SimpleJavaFileObject(classUri, kind) {
                 public OutputStream openOutputStream() throws IOException {
                     return classBytesStream;
@@ -50,16 +94,13 @@ public class CompileSourceInMemory {
     CompilationTask task = compiler.getTask(null, fileManager, diagnostics, null, null, sources);
     boolean success = task.call();
     if (success) {
-	ClassLoader transientClassLoader = new ClassLoader() {
-	    public Class<?> findClass(String name) throws ClassNotFoundException {
-	      byte[] classBytes = classBytesStream.toByteArray();
-	      return defineClass(name, classBytes, 0, classBytes.length); 
-	    }
-	};
-        Class<?> dynamicClass = Class.forName(name, true, transientClassLoader);
-        Constructor<?> constructor = dynamicClass.getConstructor(new Class[] {});
-        Object instance = constructor.newInstance(new Object[] {});
-        return dynamicClass.getMethod(methodName, new Class<?>[] {}).invoke(instance, new Object[] { });
+        ClassLoader transientClassLoader = new ClassLoader() {
+            public Class<?> findClass(String name) throws ClassNotFoundException {
+                byte[] classBytes = classBytesStream.toByteArray();
+                return defineClass(name, classBytes, 0, classBytes.length); 
+            }
+        };
+        return (Class<T>)Class.forName(fullClassName, true, transientClassLoader);
     } else {
 	    for (Diagnostic<?> diagnostic : diagnostics.getDiagnostics()) {
 	      System.out.println(diagnostic.getCode());
@@ -70,7 +111,7 @@ public class CompileSourceInMemory {
 	      System.out.println(diagnostic.getSource());
 	      System.out.println(diagnostic.getMessage(null));
 	    }
-      return "failed";
+      return null;
     }
   }
 }
